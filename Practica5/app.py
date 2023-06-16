2ea9b7c9d1ade1d34e013ff4cc9a9d0f77c77c62
JR-B07 <121039413@upq.edu.mx>
2023-06-16 17:06:48 -0600

    Practica6:preparamos vista, Enviamos datos

--- Practica5/app.py
+++ Practica5/app.py
@@ -1,5 +1,5 @@
 #importacion del framework
-from flask import Flask
+from flask import Flask,render_template,request
 from flask_mysqldb import MySQL
 
 
@@ -11,14 +11,20 @@ app.config['MYSQL_PASSWORD']='12345'
 app.config['MYSQL_DB']='dbflask'
 mysql= MySQL(app)
 
-#declaracion de ruta / http://localhost:5000
+#declaracion de ruta / http://localhost:5000 - tipo insert 
 @app.route('/')
 def index():
-    return "Hola Mundo FLASK"
+    return render_template('index.html')
 
-@app.route('/guardar')
+@app.route('/guardar',methods=['POST'])
 def guardar():
-    return "se guardo en la BD"
+    if request.method == 'POST':
+       titulo= request.form['txtTitulo']
+       artista= request.form['txtArtista'] 
+       anio= request.form['txtAnio']
+       print(titulo,artista,anio)
+         
+    return "Los datos llegaron"
 
 @app.route('/eliminar')
 def eliminar():
