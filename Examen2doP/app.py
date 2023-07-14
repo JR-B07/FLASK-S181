b857a85faf8c145cf27b56fb2f47117c7e38cc19
JR-B07 <121039413@upq.edu.mx>
2023-07-14 17:27:56 -0600

    finalizacion

--- Examen2doP/app.py
+++ Examen2doP/app.py
@@ -1,41 +1,70 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template,request, flash, redirect, url_for
 from flask_mysqldb import MySQL
 
-#inicializacion del APP
-app = Flask (__name__)
-app.config['MYSQL_HOST']='localhost'
-app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']='12345'
-app.config['MYSQL_DB']='db_flores'
+# Inicialización del APP
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '12345'
+app.config['MYSQL_DB'] = 'db_floreria'
 app.secret_key = 'mysecretkey'
 mysql = MySQL(app)
 
 @app.route('/')
 def index():
-    CC= MySQL.connection.cursor()
-    CC.execute('select * from tb_Flores')
-    conflores=CC.fetchall()
-    print(conflores)
-    return render_template('index.html', listaflores= conflores)
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM tb_flores')
+    flores = cursor.fetchall()
+    print(flores)
+    return render_template('index.html', listaflores=flores)
 
-@app.route('/guardar',methods=['POST'])
+@app.route('/guardar', methods=['POST'])
 def guardar():
     if request.method == 'POST':
-       Nombre= request.form['Nombre']
-       Cantidad= request.form['Cantidad'] 
-       Precio= request.form['Precio']
-       print(Nombre,Cantidad,Precio)
-    return "Los datos llegaron"
+        varNombre = request.form['Nombre']
+        varCantidad = request.form['Cantidad']
+        varPrecio = request.form['Precio']
+        print(varNombre, varCantidad, varPrecio)
+        cursor = mysql.connection.cursor()
+        cursor.execute('insert into tb_flores values (Nombre = %s, Cantidad = %s, Precio = %s)', (varNombre, varCantidad, varPrecio, id))
+        mysql.connection.commit()
+        cursor.close()
+    return redirect(url_for('index'))
 
-@app.route('/editar/<id>')
+@app.route('/editar/<int:id>')
 def editar(id):
-    cursorid= MySQL.connection.cursor()
-    cursorid.execute('select * from tb_flores where id= %s', (id,))
-    consulid= cursorid.fetchone()
-    print(consulid)
-    return render_template('editarFlor.html', Flor = consulid)
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM tb_flores WHERE id = %s', (id,))
+    flor = cursor.fetchone()
+    print(flor)
+    return render_template('editarFlor.html', Flor=flor)
 
+@app.route('/eliminar/<int:id>')
+def eliminar(id):
+    cursor = mysql.connection.cursor()
+    cursor.execute('DELETE FROM tb_flores WHERE id = %s', (id,))
+    mysql.connection.commit()
+    cursor.close()
 
-#ejecucion
+    flash('Se eliminó la Flor')
+    return redirect(url_for('index'))
+
+@app.route('/actualizar/<int:id>', methods=['POST'])
+def actualizar(id):
+    if request.method == 'POST':
+        varNombre = request.form['Nombre']
+        varCantidad = request.form['Cantidad']
+        varPrecio = request.form['Precio']
+
+        cursor = mysql.connection.cursor()
+        cursor.execute('UPDATE tb_flores SET Nombre = %s, Cantidad = %s, Precio = %s WHERE id = %s', (varNombre, varCantidad, varPrecio, id))
+        mysql.connection.commit()
+        cursor.close()
+
+        flash('El flores se actualizó correctamente: ' + varNombre)
+        return redirect(url_for('index'))
+
+# Ejecución
 if __name__ == '__main__':
     app.run(port=5000)
+    app.debug
